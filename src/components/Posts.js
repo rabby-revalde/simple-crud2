631f984f234c81f2d04fe61ef77b8ea07defe344
Rabby Revalde <revalde.rabbyrevalde@gmail.com>
2020-11-09 10:59:24 +0800

    update strict mode

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ListGroup, ListGroupItem, Container, Button } from "reactstrap";
 import { users } from "../assets/usersData.js";
 
